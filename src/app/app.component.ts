f9892e6006109c19471834b30c3a7379d62f8b2a
Luh.vecino <lucasvecino@gmail.com>
2025-02-21 11:55:00 -0300

    adiciona contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Contact } from '../Models/contact.model';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contactly.web';
+  private http = inject(HttpClient);
+
+  // Fix: Corrected the variable name to contacts$
+  contacts$: Observable<Contact[]> = this.getContacts();
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7035/api/Contacts');
+  }
 }
