122541ae15a21408bcff27e91fcae2886ccc5410
Luh.vecino <lucasvecino@gmail.com>
2025-02-23 13:40:48 -0300

    Projeto Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,60 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Contact } from '../Models/contact.model';
 import { AsyncPipe } from '@angular/common';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, FormsModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   private http = inject(HttpClient);
 
-  // Fix: Corrected the variable name to contacts$
-  contacts$: Observable<Contact[]> = this.getContacts();
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone: new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false),
 
-  private getContacts(): Observable<Contact[]> {
+  })
+
+  contacts$= this.getContacts();
+  
+  onFormSubmit(){
+    // console.log(this.contactsForm.value);
+    const addContactRequest = {
+      name: this.contactsForm.value.name,
+      email: this.contactsForm.value.email,
+      phone: this.contactsForm.value.phone,
+      favorite: this.contactsForm.value.favorite,
+    }
+    this.http.post('https://localhost:7035/api/Contacts', addContactRequest)
+    .subscribe({
+      next: (value) =>{
+        console.log(value);
+        this.contacts$ = this.getContacts();
+        this.contactsForm.reset();
+      }
+    })
+  }
+
+  onDelete(id: string){
+    this.http.delete(`https://localhost:7035/api/Contacts/${id}`)
+    .subscribe({
+      next: (value) => {
+        alert("Item deleted");
+        this.contacts$ = this.getContacts();
+      }
+    })
+  }
+
+  // Fix: Corrected the variable name to contacts$
+  // contacts$: Observable<Contact[]> = this.getContacts();
+
+private getContacts(): Observable<Contact[]> {
     return this.http.get<Contact[]>('https://localhost:7035/api/Contacts');
   }
 }
